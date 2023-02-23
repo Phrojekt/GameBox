bf7ed76394ca1ec85aa40a6443cbba3d9f49266f
Phrojekt <phrojekt22@gmail.com>
2023-02-23 17:57:35 -0300

    Ajustes

--- GameList.js
+++ GameList.js
@@ -1,11 +1,16 @@
 const modal = document.querySelector('.ScreenModal');
 
 const Modal = () => {
+
+    const selectedItem = document.getElementById('game1');
+
     if(modal.classList.contains('active')) {
         modal.classList.remove('active')
+        const selected = document.querySelector('.selected')
+        selected.classList.remove('selected');
+        
     } else {
         modal.classList.add('active')
-        const selectedItem = document.getElementById('game1')
         selectedItem.classList.add('selected');
     }
 
